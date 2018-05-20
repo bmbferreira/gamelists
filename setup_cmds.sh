46761ad47ea301e4eff1786ce416b6abf9da1916
Connor Rigby <konnorrigby@gmail.com>
2018-05-19 21:27:43 -0700

    Add distillery.

--- setup_cmds.sh
+++ setup_cmds.sh
@@ -1,4 +1,5 @@
 source ./.env
 gigalixir create_database --free $APP_NAME
-gigalixir add_ssh_key "ssh-rsa $SSH_KEY $EMAIL"
+# gigalixir add_ssh_key "ssh-rsa $SSH_KEY $EMAIL"
+gigalixir add_ssh_key "$(cat ~/.ssh/id_rsa.pub)"
 gigalixir set_config $APP_NAME SECRET_KEY_BASE $SECRET_KEY_BASE
